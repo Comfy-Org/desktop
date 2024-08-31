f272374dcd455ce07a2560d8acbbf21c3d31c292
Kendal Cormany <kendal.cormany@nexus.io>
2024-08-31 11:57:03 -0700

    Trying Another shim to unpack

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,3 +1,4 @@
 cp -f scripts/shims/utils.js node_modules/@electron/osx-sign/dist/cjs/util.js
+cp -f scripts/shims/mac.js node_modules/electron/packager/dist/mac.js
 
 cat node_modules/@electron/osx-sign/dist/cjs/util.js
