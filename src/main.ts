dd56091723baf6a837cbdad9cd1765bd9ade3d61
Kendal Cormany <kendal.cormany@nexus.io>
2024-09-10 20:08:22 -0700

    Fixed dev mode

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,13 @@ const createWindow = () => {
 
   // Load the UI from the Python server's URL
   //mainWindow.loadURL('http://localhost:8188/');
-  mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
 
   // Set up the System Tray Icon for all platforms
   // Returns a tray so you can set a global var to access.
