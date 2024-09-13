94143e035427026ba695a02887bcfe27568370fe
Robin Huang <robin.j.huang@gmail.com>
2024-09-13 10:57:25 +0900

    Lint all files.,

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { spawn, ChildProcess } from 'node:child_process';
 import * as fsPromises from 'node:fs/promises';
-import fs from 'fs'
+import fs from 'fs';
 import net from 'node:net';
 import path from 'node:path';
 import { SetupTray } from './tray';
-import { IPC_CHANNELS } from './constants';
-import dotenv from "dotenv";
-import { app, BrowserWindow, webContents, ipcMain, screen } from 'electron';
+
+import dotenv from 'dotenv';
+import { app, BrowserWindow, webContents } from 'electron';
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-import('electron-squirrel-startup').then(ess => {
-  const {default: check} = ess;
+import('electron-squirrel-startup').then((ess) => {
+  const { default: check } = ess;
   if (check) {
     app.quit();
   }
@@ -40,7 +40,9 @@ const createWindow = async () => {
     console.log('Loading Vite Dev Server');
     await mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    await mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Set up the System Tray Icon for all platforms
@@ -49,14 +51,14 @@ const createWindow = async () => {
 
   // Overrides the behavior of closing the window to allow for
   // the python server to continue to run in the background
-  mainWindow.on('close' , (e:Electron.Event) => {
+  mainWindow.on('close', (e: Electron.Event) => {
     e.preventDefault();
     mainWindow.hide();
     // Mac Only Behavior
     if (process.platform === 'darwin') {
       app.dock.hide();
     }
-  })
+  });
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 };
@@ -68,9 +70,9 @@ async function serverHeartBeat() {
   const isReady = await isPortInUse(host, port);
   if (isReady) {
     // Getting webcontents[0] is not reliable if app started with dev window
-    webContents.getAllWebContents()[0].send("python-server-status", "active");
+    webContents.getAllWebContents()[0].send('python-server-status', 'active');
   } else {
-    webContents.getAllWebContents()[0].send("python-server-status", "false");
+    webContents.getAllWebContents()[0].send('python-server-status', 'false');
   }
 }
 
@@ -100,8 +102,11 @@ const maxFailWait: number = 50 * 1000; // 50seconds
 let currentWaitTime = 0;
 const spawnServerTimeout: NodeJS.Timeout = null;
 
-const launchPythonServer = async (args: {userResourcesPath: string, appResourcesPath: string}) => {
-  const {userResourcesPath, appResourcesPath} = args;
+const launchPythonServer = async (args: {
+  userResourcesPath: string;
+  appResourcesPath: string;
+}) => {
+  const { userResourcesPath, appResourcesPath } = args;
 
   const isServerRunning = await isPortInUse(host, port);
   if (isServerRunning) {
@@ -109,11 +114,14 @@ const launchPythonServer = async (args: {userResourcesPath: string, appResources
     // Server has been started outside the app, so attach to it.
     setTimeout(() => {
       // Not sure if needed but wait a few moments before sending the connect message up.
-      webContents.getAllWebContents()[0].send("python-server-status", "active");
+      webContents.getAllWebContents()[0].send('python-server-status', 'active');
     }, 5000);
     clearInterval(serverHeartBeatReference);
     webContents.getAllWebContents()[0].loadURL('http://localhost:8188/');
-    serverHeartBeatReference = setInterval(serverHeartBeat, serverHeartBeatInterval);
+    serverHeartBeatReference = setInterval(
+      serverHeartBeat,
+      serverHeartBeatInterval
+    );
     return Promise.resolve();
   }
 
@@ -121,20 +129,30 @@ const launchPythonServer = async (args: {userResourcesPath: string, appResources
 
   return new Promise<void>(async (resolve, reject) => {
     const pythonRootPath = path.join(userResourcesPath, 'python');
-    const pythonInterpreterPath = process.platform==='win32' ? path.join(pythonRootPath, 'python.exe') : path.join(pythonRootPath, 'bin', 'python');
-    const pythonRecordPath = path.join(pythonRootPath, "INSTALLER");
+    const pythonInterpreterPath =
+      process.platform === 'win32'
+        ? path.join(pythonRootPath, 'python.exe')
+        : path.join(pythonRootPath, 'bin', 'python');
+    const pythonRecordPath = path.join(pythonRootPath, 'INSTALLER');
     const scriptPath = path.join(appResourcesPath, 'ComfyUI', 'main.py');
     const userDirectoryPath = path.join(app.getPath('userData'), 'user');
     const inputDirectoryPath = path.join(app.getPath('userData'), 'input');
     const outputDirectoryPath = path.join(app.getPath('userData'), 'output');
-    const comfyMainCmd = [scriptPath,
-        '--user-directory', userDirectoryPath,
-        '--input-directory', inputDirectoryPath,
-        '--output-directory', outputDirectoryPath,
-        ...(process.env.COMFYUI_CPU_ONLY === "true" ? ["--cpu"] : [])];
+    const comfyMainCmd = [
+      scriptPath,
+      '--user-directory',
+      userDirectoryPath,
+      '--input-directory',
+      inputDirectoryPath,
+      '--output-directory',
+      outputDirectoryPath,
+      ...(process.env.COMFYUI_CPU_ONLY === 'true' ? ['--cpu'] : []),
+    ];
 
     const spawnPython = (cmd: string[], cwd: string) => {
-      const pythonProcess: ChildProcess = spawn(pythonInterpreterPath, cmd, {cwd});
+      const pythonProcess: ChildProcess = spawn(pythonInterpreterPath, cmd, {
+        cwd,
+      });
 
       pythonProcess.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
@@ -143,12 +161,15 @@ const launchPythonServer = async (args: {userResourcesPath: string, appResources
         console.log(`stdout: ${data}`);
       });
 
-      return pythonProcess
-    }
+      return pythonProcess;
+    };
 
     try {
       // check for existence of both interpreter and INSTALLER record to ensure a correctly installed python env
-      await Promise.all([fsPromises.access(pythonInterpreterPath), fsPromises.access(pythonRecordPath)]);
+      await Promise.all([
+        fsPromises.access(pythonInterpreterPath),
+        fsPromises.access(pythonRecordPath),
+      ]);
       pythonProcess = spawnPython(comfyMainCmd, path.dirname(scriptPath));
     } catch {
       console.log('Running one-time python installation on first startup...');
@@ -162,16 +183,28 @@ const launchPythonServer = async (args: {userResourcesPath: string, appResources
 
       const wheelsPath = path.join(pythonRootPath, 'wheels');
       // TODO: report space bug to uv upstream, then revert below mac fix
-      const rehydrateCmd = ['-m', ...(process.platform !== 'darwin' ? ['uv'] : []),  'pip', 'install', '--no-index', '--no-deps', ...(await fsPromises.readdir(wheelsPath)).map(x => path.join(wheelsPath, x))];
-      const rehydrateProc = spawn(pythonInterpreterPath, rehydrateCmd, {cwd: wheelsPath});
+      const rehydrateCmd = [
+        '-m',
+        ...(process.platform !== 'darwin' ? ['uv'] : []),
+        'pip',
+        'install',
+        '--no-index',
+        '--no-deps',
+        ...(await fsPromises.readdir(wheelsPath)).map((x) =>
+          path.join(wheelsPath, x)
+        ),
+      ];
+      const rehydrateProc = spawn(pythonInterpreterPath, rehydrateCmd, {
+        cwd: wheelsPath,
+      });
 
-      rehydrateProc.on("exit", code => {
-        if (code===0) {
+      rehydrateProc.on('exit', (code) => {
+        if (code === 0) {
           // write an INSTALLER record on sucessful completion of rehydration
-          fsPromises.writeFile(pythonRecordPath, "ComfyUI");
+          fsPromises.writeFile(pythonRecordPath, 'ComfyUI');
 
           // remove the now installed wheels
-          fsPromises.rm(wheelsPath, {recursive: true});
+          fsPromises.rm(wheelsPath, { recursive: true });
           console.log(`Python successfully installed to ${pythonRootPath}`);
 
           pythonProcess = spawnPython(comfyMainCmd, path.dirname(scriptPath));
@@ -188,15 +221,20 @@ const launchPythonServer = async (args: {userResourcesPath: string, appResources
       if (currentWaitTime > maxFailWait) {
         //Something has gone wrong and we need to backout.
         clearTimeout(spawnServerTimeout);
-        reject("Python Server Failed To Start");
+        reject('Python Server Failed To Start');
       }
       const isReady = await isPortInUse(host, port);
       if (isReady) {
         sendProgressUpdate(90, 'Finishing...');    
         console.log('Python server is ready');
         // Start the Heartbeat listener, send connected message to Renderer and resolve promise.
-        serverHeartBeatReference = setInterval(serverHeartBeat, serverHeartBeatInterval);
-        webContents.getAllWebContents()[0].send("python-server-status", "active");
+        serverHeartBeatReference = setInterval(
+          serverHeartBeat,
+          serverHeartBeatInterval
+        );
+        webContents
+          .getAllWebContents()[0]
+          .send('python-server-status', 'active');
         //For now just replace the source of the main window to the python server
         setTimeout( () => webContents.getAllWebContents()[0].loadURL('http://localhost:8188/'), 1000);
         clearTimeout(spawnServerTimeout);
@@ -228,7 +266,7 @@ app.on('ready', async () => {
   console.log(`appResourcesPath: ${appResourcesPath}`);
 
   try {
-    dotenv.config({path: path.join(appResourcesPath, "ComfyUI", ".env")});
+    dotenv.config({ path: path.join(appResourcesPath, 'ComfyUI', '.env') });
   } catch {
     // if no .env file, skip it
   }
@@ -262,20 +300,16 @@ const killPythonServer = () => {
   return new Promise<void>((resolve, reject) => {
     if (pythonProcess) {
       try {
-        const result:boolean = pythonProcess.kill(); //false if kill did not succeed sucessfully
+        const result: boolean = pythonProcess.kill(); //false if kill did not succeed sucessfully
         result ? resolve() : reject();
-      }
-      catch(error)
-      {
+      } catch (error) {
         console.error(error);
         reject(error);
       }
-    }
-    else
-    {
+    } else {
       resolve();
     }
-  })
+  });
 };
 
 type DirectoryStructure = (string | [string, string[]])[];
@@ -288,7 +322,9 @@ function createComfyDirectories(): void {
     'input',
     'output',
     'user',
-    ['models', [
+    [
+      'models',
+      [
         'checkpoints',
         'clip',
         'clip_vision',
@@ -306,7 +342,8 @@ function createComfyDirectories(): void {
         'upscale_models',
         'vae',
         'vae_approx',
-    ]]
+      ],
+    ],
   ];
 
   function createDir(dirPath: string): void {
@@ -337,18 +374,16 @@ function createComfyDirectories(): void {
 app.on('before-quit', async () => {
   try {
     await killPythonServer();
-  }
-  catch(error)
-  {
+  } catch (error) {
     // Server did NOT exit properly
     app.exit();
   }
   app.exit();
-})
+});
 
 app.on('quit', () => {
   app.exit();
-})
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
