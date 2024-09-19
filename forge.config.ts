06111049c8c01edc8908b5146ffeec4aa7548736
Robin Huang <robin.j.huang@gmail.com>
2024-09-19 11:55:32 +0900

    back to s3.

--- forge.config.ts
+++ forge.config.ts
@@ -82,14 +82,14 @@ const config: ForgeConfig = {
     new MakerSquirrel(
       (arch) => ({
         noDelta: !process.env.PUBLISH,
-        remoteReleases: `https://updater.comfy.org/win32/${arch}`,
+        remoteReleases: `https://comfyui-electron-releases.s3.us-west-2.amazonaws.com/win32/${arch}`,
         frameworkVersion: 'net481',
       }),
       ['win32']
     ),
     new MakerZIP(
       (arch) => ({
-        macUpdateManifestBaseUrl: `https://updater.comfy.org/darwin/${arch}`,
+        macUpdateManifestBaseUrl: `https://comfyui-electron-releases.s3.us-west-2.amazonaws.com/darwin/${arch}`,
       }),
       ['darwin', 'win32']
     ),
