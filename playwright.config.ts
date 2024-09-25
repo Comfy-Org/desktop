b92b16261e3963b188bbf5ed1e2f5db8f5d76186
Robin Huang <robin.j.huang@gmail.com>
2024-09-25 16:09:54 +0900

    Fix test paths.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testMatch: './src/__tests__/e2e/**/*.test.ts',
-  snapshotDir: 'src/__tests__/e2e/snapshots',
+  testMatch: 'src/__tests__/e2e/*.test.ts',
 });
