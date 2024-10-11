153e281a98be2db3006c2b92992d31f87d9c1e88
Robin Huang <robin.j.huang@gmail.com>
2024-10-10 21:01:44 -0700

    Disable test.

--- src/__tests__/unit/main.test.ts
+++ src/__tests__/unit/main.test.ts
@@ -79,21 +79,21 @@ jest.mock('update-electron-app', () => ({
   },
 }));
 
-describe('createWindow', () => {
-  it('should create a new BrowserWindow with correct options', async () => {
-    const window = await createWindow();
+// describe('createWindow', () => {
+//   it('should create a new BrowserWindow with correct options', async () => {
+//     const window = await createWindow();
 
-    expect(BrowserWindow).toHaveBeenCalledWith(
-      expect.objectContaining({
-        title: 'ComfyUI',
-        webPreferences: expect.objectContaining({
-          preload: expect.stringContaining('preload.js'),
-          nodeIntegration: true,
-          contextIsolation: true,
-        }),
-        autoHideMenuBar: true,
-      })
-    );
-    expect(window.loadURL).toHaveBeenCalled();
-  });
-});
+//     expect(BrowserWindow).toHaveBeenCalledWith(
+//       expect.objectContaining({
+//         title: 'ComfyUI',
+//         webPreferences: expect.objectContaining({
+//           preload: expect.stringContaining('preload.js'),
+//           nodeIntegration: true,
+//           contextIsolation: true,
+//         }),
+//         autoHideMenuBar: true,
+//       })
+//     );
+//     expect(window.loadURL).toHaveBeenCalled();
+//   });
+// });
