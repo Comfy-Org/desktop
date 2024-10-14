39a961878fffdb1190d68f5e0ce6ddc8aaf7c5e4
Robin Huang <robin.j.huang@gmail.com>
2024-10-14 12:12:06 -0700

    Get GPU information for Sentry.

--- src/main.ts
+++ src/main.ts
@@ -69,6 +69,20 @@ if (!gotTheLock) {
       ],
     });
 
+  app
+    .getGPUInfo('complete')
+    .then((gpuInfo: any) => {
+      Sentry.setContext('gpu', {
+        vendor: gpuInfo.auxAttributes?.glVendor || 'Unknown',
+        renderer: gpuInfo.auxAttributes?.glRenderer || 'Unknown',
+        gpuDevice: gpuInfo.gpuDevice,
+        featureStatus: gpuInfo.featureStatus,
+      });
+    })
+    .catch((e) => {
+      log.error('Error getting GPU info: ', e);
+    });
+
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
