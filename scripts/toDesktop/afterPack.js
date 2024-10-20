8ce13ccd5b7bb2fe4b3c889fdb92f0b3a96cbb64
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-20 12:40:05 -0700

    Added appID from env

---
+++ scripts/toDesktop/afterPack.js
@@ -0,0 +1,8 @@
+module.exports = async ({ pkgJsonPath, pkgJson, appDir, hookName }) => {
+    /**
+     * pkgJsonPath - string - path to the package.json file
+     * pkgJson - object - the parsed package.json file
+     * appDir - string - the path to the app directory
+     * hookName - string - the name of the hook ("todesktop:beforeInstall" or "todesktop:afterPack")
+     */
+  };
