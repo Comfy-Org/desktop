a16301702a8034b06635f3c0bda971422bd1b5c0
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-22 22:49:04 -0700

    electron-builder for debug

---
+++ builder-debug.config.ts
@@ -0,0 +1,22 @@
+import { Configuration } from "electron-builder";
+
+const debugConfig: Configuration = {
+    files: ["package.json","README.md","src/**", ".vite/**",],
+    extraResources: ['./assets/ComfyUI', './assets/python.tgz', './assets/UI'],
+    win: {
+        icon: './assets/UI/Comfy_Logo.ico',
+        target: 'zip',
+        signtoolOptions: null
+    },
+    mac: {
+        icon: './assets/UI/Comfy_Logo.icns',
+        target: "zip",
+        sign: null,
+    },
+    linux: {
+        icon: './assets/UI/Comfy_Logo_x128.png',
+        target: "appimage"
+    }
+}
+
+export default debugConfig;
