307ff87fcf17de5a7ad142d59c433070a164e239
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-22 17:13:08 -0700

    Try this for python sign

--- scripts/todesktop/beforeInstall.js
+++ scripts/todesktop/beforeInstall.js
@@ -1,9 +1,21 @@
+const { exec } = require("child_process");
+const path = require("path");
 
 module.exports = async ({ pkgJsonPath, pkgJson, appDir, hookName }) => {
-      /**
-   * pkgJsonPath - string - path to the package.json file
-   * pkgJson - object - the parsed package.json file
-   * appDir - string - the path to the app directory
-   * hookName - string - the name of the hook ("todesktop:beforeInstall" or "todesktop:afterPack")
-   */
+    /**
+ * pkgJsonPath - string - path to the package.json file
+ * pkgJson - object - the parsed package.json file
+ * appDir - string - the path to the app directory
+ * hookName - string - the name of the hook ("todesktop:beforeInstall" or "todesktop:afterPack")
+ */
+
+    if (process.platform === "darwin") {
+        const result = exec(`sh ${path.join(appDir, 'scripts', 'signPyhton.sh')}`, (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+    }
 };
