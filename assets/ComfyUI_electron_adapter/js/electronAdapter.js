fba4c069ad5f2158f6a8a804a970871dcaf7daf2
huchenlei <huchenlei@proton.me>
2024-10-25 13:46:44 -0400

    Add testing extension

---
+++ assets/ComfyUI_electron_adapter/js/electronAdapter.js
@@ -0,0 +1,13 @@
+import { app } from "../../scripts/app.js";
+
+app.registerExtension({
+  name: "Comfy.ElectronAdapter",
+  settings: [
+    {
+      id: 'Comfy.Electron.Foo',
+      name: 'A dummy setting to verify that the extension is loaded',
+      type: 'hidden',
+      defaultValue: true,
+    },
+  ],
+});
