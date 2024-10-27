0223866b1f6686dce83790138358386f57c88d85
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-26 19:43:21 -0700

    remove .git this way

--- scripts/todesktop/afterPack.js
+++ scripts/todesktop/afterPack.js
@@ -29,6 +29,7 @@ module.exports = async ({ appOutDir, packager,outDir }) => {
     const mainPath = path.dirname(outDir);
     const assetPath = path.join(mainPath,'app-wrapper','app','assets');
     const resourcePath = path.join(path.dirname(appPath),"resources");
+    await fs.rm(path.join(assetPath,"ComfyUI",".git"),{recursive:true,force:true});
     await fs.cp(assetPath, resourcePath, {recursive: true});
   }
 }
