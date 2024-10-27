9221dbbf6532f30bfc95534213487a850ae67750
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-26 19:26:25 -0700

    changed sign command

--- scripts/signPython.sh
+++ scripts/signPython.sh
@@ -1,13 +1,17 @@
 python -m pip install --upgrade pip
+wait
 pip install comfy-cli
+wait
 ls
-cd ..
 cd assets
 comfy --skip-prompt --here install --fast-deps --m-series --manager-url https://github.com/Comfy-Org/manager-core
+wait
 cd ComfyUI
 cd .. 
 comfy --here standalone --platform macos
+wait
 comfy standalone --rehydrate
+wait
 rm -rf ComfyUI/custom_nodes/ComfyUI-Manager/.git
 mkdir python2/
 tar -xzf python.tgz -C python2/
@@ -15,7 +19,7 @@ rm python.tgz
 echo Sign Libs and Bins
 cd python2/python/
 filelist=("lib/libpython3.12.dylib" "lib/python3.12/lib-dynload/_crypt.cpython-312-darwin.so" "bin/uv" "bin/uvx" "bin/python3.12")
-for file in ${filelist[@]}; do codesign --sign 6698D856280DC1662A8E01E5B63428CB6D6651BB --force --timestamp --options runtime --entitlements ../../../scripts/entitlements.mac.plist "$file"; done
+for file in ${filelist[@]}; do codesign --sign "Developer ID Application: Drip Artificial Inc" --force --timestamp --options runtime --entitlements ../../../scripts/entitlements.mac.plist "$file"; done
 echo Rezip
 cd ../..
 mv python python3 
