9af275b1db8b609ecf434a516cb38ec332d73df6
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-27 17:05:35 -0700

    Move some files out then put them back in

--- scripts/signPython.sh
+++ scripts/signPython.sh
@@ -22,16 +22,15 @@ find . -name '*.tar.gz' -delete
 wait
 ls
 echo Sign Libs and Bins
-cd python2/python/
-filelist=("lib/libpython3.12.dylib" "lib/python3.12/lib-dynload/_crypt.cpython-312-darwin.so" "bin/uv" "bin/uvx" "bin/python3.12")
-for file in ${filelist[@]}; do codesign --sign 48F7750D708A9F1506A3DD335ADE537ED2FEA768 --force --timestamp --options runtime --entitlements ../../../builderStaticFiles/entitlements.mac.plist "$file"; done
-echo Rezip
-cd ../..
 mv python python3 
 mv python2/python python
-tar -czf python.tgz python/
+mkdir output
 rm -rf python2
 rm -rf python3
+cd python
+filelist=("lib/libpython3.12.dylib" "lib/python3.12/lib-dynload/_crypt.cpython-312-darwin.so" "bin/uv" "bin/uvx" "bin/python3.12")
+for file in ${filelist[@]}; do mkdir -p `dirname ../output/$file` && mv "$file" ../output/; done
+cd ..
+echo Rezip
+tar -czf python.tgz python/
 rm -rf python
-rm python.tgz
-ls
