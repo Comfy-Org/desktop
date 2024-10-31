47bf872c15fa778d69ce3965411fa59330f06ab8
Robin Huang <robin.j.huang@gmail.com>
2024-10-31 13:13:54 -0700

    Fix.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { spawn, ChildProcess } from 'node:child_process';
-import * as fsPromises from 'node:fs/promises';
 import fs from 'fs';
 import axios from 'axios';
 import path from 'node:path';
@@ -18,7 +17,6 @@ import Store from 'electron-store';
 import * as net from 'net';
 import { graphics } from 'systeminformation';
 import { createModelConfigFiles, readBasePathFromConfig } from './config/extra_model_config';
-import { WebSocketServer } from 'ws';
 import { StoreType } from './store';
 import todesktop from '@todesktop/runtime';
 import { PythonEnvironment } from './pythonEnvironment';
@@ -31,7 +29,6 @@ let isRestarting: boolean = false; // Prevents double restarts TODO(robinhuang):
 const host = '127.0.0.1';
 let port = 8188;
 let mainWindow: BrowserWindow | null = null;
-let wss: WebSocketServer | null;
 let store: Store<StoreType> | null = null;
 const messageQueue: Array<any> = []; // Stores mesaages before renderer is ready.
 let downloadManager: DownloadManager;
