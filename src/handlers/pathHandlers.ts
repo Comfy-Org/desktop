75d64dc5bc070de610cc6d7af4580d8f8be5d58d
Robin Huang <robin.j.huang@gmail.com>
2024-11-09 14:53:29 -0800

    Fix imports.

--- src/handlers/pathHandlers.ts
+++ src/handlers/pathHandlers.ts
@@ -1,27 +1,26 @@
 import { app, ipcMain, shell } from 'electron';
 import { IPC_CHANNELS } from '../constants';
-import { determineResourcesPaths } from '../main';
 import log from 'electron-log/main';
 import { getModelConfigPath } from '../config/extra_model_config';
+import { getBasePath } from '../install/resourcePaths';
 
 export class PathHandlers {
   constructor() {}
 
   registerHandlers() {
-    ipcMain.on(IPC_CHANNELS.OPEN_LOGS_PATH, () => {
+    ipcMain.on(IPC_CHANNELS.OPEN_LOGS_PATH, (): void => {
       shell.openPath(app.getPath('logs'));
     });
 
-    ipcMain.handle(IPC_CHANNELS.GET_MODEL_CONFIG_PATH, () => {
+    ipcMain.handle(IPC_CHANNELS.GET_MODEL_CONFIG_PATH, (): string => {
       return getModelConfigPath();
     });
 
-    ipcMain.handle(IPC_CHANNELS.GET_BASE_PATH, async () => {
-      const { basePath } = await determineResourcesPaths();
-      return basePath;
+    ipcMain.handle(IPC_CHANNELS.GET_BASE_PATH, async (): Promise<string | null> => {
+      return getBasePath();
     });
 
-    ipcMain.on(IPC_CHANNELS.OPEN_PATH, (event, folderPath: string) => {
+    ipcMain.on(IPC_CHANNELS.OPEN_PATH, (event, folderPath: string): void => {
       log.info(`Opening path: ${folderPath}`);
       shell.openPath(folderPath);
     });
