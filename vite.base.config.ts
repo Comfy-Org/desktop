849749327bcd5914bfde125be03051233efdf499
huchenlei <huchenlei@proton.me>
2024-11-14 09:05:05 -0500

    Fix type errors

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -10,11 +10,10 @@ export const external = [
   ...Object.keys('dependencies' in pkg ? (pkg.dependencies as Record<string, unknown>) : {}),
 ];
 
-export function getBuildConfig(env: ConfigEnv<'build'>): UserConfig {
-  const { root, mode, command } = env;
+export function getBuildConfig(env: ConfigEnv): UserConfig {
+  const { mode, command } = env;
 
   return {
-    root,
     mode,
     build: {
       // Prevent multiple builds from interfering with each other.
@@ -33,10 +32,7 @@ export function pluginHotRestart(command: 'reload' | 'restart'): Plugin {
     name: '@electron-forge/plugin-vite:hot-restart',
     closeBundle() {
       if (command === 'reload') {
-        for (const server of Object.values(process.viteDevServers)) {
-          // Preload scripts hot reload.
-          server.ws.send({ type: 'full-reload' });
-        }
+        // TODO: Send message to external renderer dev server for reload.
       } else {
         // Main process hot restart.
         // https://github.com/electron/forge/blob/v7.2.0/packages/api/core/src/api/start.ts#L216-L223
