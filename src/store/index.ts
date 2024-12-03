4f27889919ebe7f89c482bbd3993ea8fafb091f5
huchenlei <huchenlei@proton.me>
2024-12-03 18:43:50 -0500

    Add documentation

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,5 +8,12 @@ export type AppWindowSettings = {
 
 export type DesktopSettings = {
   basePath?: string;
+  /**
+   * The state of the installation.
+   * - `started`: The installation has started.
+   * - `installed`: A fresh installation.
+   * - `upgraded`: An upgrade from a previous version that stores the base path
+   * in the yaml config.
+   */
   installState?: 'started' | 'installed' | 'upgraded';
 };
