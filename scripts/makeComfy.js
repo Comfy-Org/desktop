36d85df2cc3c3f703b05dffc2332b070425eec52
huchenlei <huchenlei@proton.me>
2025-01-14 17:01:25 -0500

    nit

--- scripts/makeComfy.js
+++ scripts/makeComfy.js
@@ -4,7 +4,7 @@ import { execSync } from 'node:child_process';
 const comfyRepo = 'https://github.com/comfyanonymous/ComfyUI';
 const managerRepo = 'https://github.com/Comfy-Org/ComfyUI-Manager';
 
-execSync(`git clone ${comfyRepo} --depth 1 --branch ${pkg.config.comfyVersion} assets/ComfyUI`);
+execSync(`git clone ${comfyRepo} --depth 1 --branch v${pkg.config.comfyVersion} assets/ComfyUI`);
 execSync(`git clone ${managerRepo} assets/ComfyUI/custom_nodes/ComfyUI-Manager`);
 execSync(`cd assets/ComfyUI/custom_nodes/ComfyUI-Manager && git checkout ${pkg.config.managerCommit} && cd ../../..`);
 execSync(`yarn run make:frontend`);
