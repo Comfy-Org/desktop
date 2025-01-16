Comfy-Org/desktop

commit 8426150b793bb68aaf733b5e91dc402f5a1bd60b

Fix uv shell never exits (#558)

src/virtualEnvironment.ts
@@ -108,17 +108,8 @@ export class VirtualEnvironment {
     try {
       await this.createEnvironment(callbacks);
     } finally {
-      if (this.uvPty) {
-        // If we have a pty instance then we need to kill it on a delay
-        // else you may get an EPIPE error on reading the stream if it is
-        // reading/writing as you kill it
-        const pty = this.uvPty;
-        this.uvPty = undefined;
-        pty.pause();
-        setTimeout(() => {
-          this.uvPty?.kill();
-        }, 100);
-      }
+      const pid = this.uvPty?.pid;
+      if (pid) process.kill(pid);
     }
   }
 

commit 05a3afc3c867d2dafe24c2a33a56bbcbf163d7d4

Fix regression: uv termianl cannot be restarted
Introduced in #558

src/virtualEnvironment.ts
@@ -119,7 +119,10 @@ export class VirtualEnvironment implements HasTelemetry {
       await this.createEnvironment(callbacks);
     } finally {
       const pid = this.uvPty?.pid;
-      if (pid) process.kill(pid);
+      if (pid) {
+        process.kill(pid);
+        this.uvPty = undefined;
+      }
     }
   }
 