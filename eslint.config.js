1e9908cad26349323922c577d82ef44463c48bf5
christian-byrne <abolkonsky.rem@gmail.com>
2025-01-22 17:28:45 -0700

    apply last no-unsafe- rule to test files

--- eslint.config.js
+++ eslint.config.js
@@ -74,6 +74,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   }
