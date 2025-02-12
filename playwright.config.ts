97a812c0c904918b30fc8ea9418533dd061e3f7e
filtered <176114999+webfiltered@users.noreply.github.com>
2025-02-12 16:32:30 +1100

    Set more realistic timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,4 +4,13 @@ export default defineConfig({
   testDir: './tests/integration',
   /* Run local instance before starting the tests */
   globalSetup: './playwright.setup',
+  // Per-test timeout - 60 sec
+  timeout: 60_000,
+  // Entire test suite timeout - 1 hour
+  globalTimeout: 60 * 60 * 1000,
+  expect: {
+    timeout: 10_000,
+  },
+  // This is a desktop app; sharding is required to run tests in parallel.
+  workers: 1,
 });
