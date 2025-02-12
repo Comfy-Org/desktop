be7f2394e842cd0ed044de7a7430b0ebab3c65c5
filtered <176114999+webfiltered@users.noreply.github.com>
2025-02-12 16:15:15 +1100

    Backup local user config on global setup

--- playwright.setup.ts
+++ playwright.setup.ts
@@ -1,5 +1,22 @@
-function globalSetup() {
+import { rename } from 'node:fs/promises';
+
+import { FilePermission, addRandomSuffix, getComfyUIAppDataPath, pathExists } from './tests/shared/utils';
+
+async function globalSetup() {
   console.log('+ Playwright globalSetup called');
+  if (process.env.CI) return;
+
+  const appDataPath = getComfyUIAppDataPath();
+  await backupByRenaming(appDataPath);
+}
+
+async function backupByRenaming(appDataPath: string) {
+  if (!(await pathExists(appDataPath, FilePermission.Writable))) return;
+
+  const newPath = addRandomSuffix(appDataPath);
+  console.warn(`AppData exists! Moving ${appDataPath} to ${newPath}. Remove manually if you do not require it.`);
+  await rename(appDataPath, newPath);
+  return newPath;
 }
 
 export default globalSetup;
