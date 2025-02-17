a65ab1bd8f390d750f6ab71206350cc13dad80da
Robin Huang <robin.j.huang@gmail.com>
2025-02-17 11:29:37 -0800

    Remove git username.

--- scripts/makeComfy.js
+++ scripts/makeComfy.js
@@ -5,14 +5,9 @@ import pkg from './getPackage.js';
 const comfyRepo = 'https://github.com/comfyanonymous/ComfyUI';
 const managerRepo = 'https://github.com/Comfy-Org/ComfyUI-Manager';
 
-// Needed to do cherry-picks
-execSync('git config --global user.email "build-bot@example.com"');
-execSync('git config --global user.name "Build Bot"');
-
 // Clone and checkout base versions
 execSync(`git clone ${comfyRepo} --depth 1 --branch ${pkg.config.comfyBranch} assets/ComfyUI`);
 execSync(`git clone ${managerRepo} assets/ComfyUI/custom_nodes/ComfyUI-Manager`);
 execSync(`cd assets/ComfyUI/custom_nodes/ComfyUI-Manager && git checkout ${pkg.config.managerCommit} && cd ../../..`);
-
 execSync(`yarn run make:frontend`);
 execSync(`yarn run download:uv all`);
