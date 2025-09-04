614516d8ff6251c30af080026ba78605388b7872
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-04 21:09:05 +1000

    Complete refactoring of install stages with proper naming

--- src/virtualEnvironment.ts
+++ src/virtualEnvironment.ts
@@ -303,7 +303,9 @@ export class VirtualEnvironment implements HasTelemetry {
 
   @trackEvent('install_flow:virtual_environment_install_requirements')
   public async installRequirements(callbacks?: ProcessCallbacks): Promise<void> {
-    useAppState().setInstallStage(createInstallStageInfo(InstallStage.INSTALLING_COMFYUI_REQUIREMENTS, { progress: 35 }));
+    useAppState().setInstallStage(
+      createInstallStageInfo(InstallStage.INSTALLING_COMFYUI_REQUIREMENTS, { progress: 35 })
+    );
 
     // pytorch nightly is required for MPS
     if (process.platform === 'darwin') {
